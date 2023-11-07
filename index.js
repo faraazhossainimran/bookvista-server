72ddee0432d2d1d06a06980803a5a967a248563c
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-11-08 01:38:10 +0600

    User base specific bookings added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eihrzue.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,27 +27,51 @@ async function run() {
     const roomsCollection = client.db("bookvista").collection("rooms");
     const bookingCollection = client.db("bookvista").collection("booking");
     // get all rooms data
-    app.get('/rooms', async(req, res) => {
-        const result = await roomsCollection.find().toArray();
-        res.send(result)
-    })
+    app.get("/rooms", async (req, res) => {
+      const result = await roomsCollection.find().toArray();
+      res.send(result);
+    });
     // get single room data
-    app.get('/rooms/:id', async(req, res)=>{
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) };
-        const result = await roomsCollection.findOne(query);
-        res.send(result)
-    })
+    app.get("/rooms/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    });
     // book room
-    app.post('/bookings', async(req, res)=>{
+    app.post("/bookings", async (req, res) => {
       const booking = req.body;
-      console.log('from booking', booking);
+      console.log("from booking", booking);
       const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+    app.get('/bookings', async(req, res)=> {
+      console.log(req.query.email);
+      let query = {};
+      if(req.query?.email){
+        query = {userEmail: req.query.email}
+      }
+      const result = await bookingCollection.find(query).toArray()
       res.send(result)
-  })
+    })
+    // get a booked room
+    app.get("/bookings", async (req, res) => {
+      const result = await bookingCollection.find().toArray();
+      res.send(result);
+    });
+    // Delete a specific bookedroom
+    app.delete('/bookings/:id', async(req, res)=> {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      console.log(result);
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -56,12 +79,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send(`coffee server is running`);
+});
 
-app.get('/', (req, res)=> {
-    res.send(`coffee server is running`)
-})
-
-
-app.listen(port, ()=> {
-    console.log(`port is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`port is running on port ${port}`);
+});
